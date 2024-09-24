68ea51c7d6a72710bd62db5c2a6bf2f649a4a79e
shogun0331 <shogun0331@gmail.com>
2024-09-24 15:29:06 +0900

    UTween 유틸 적용 및 UTween 시퀀스 플레이 Flow 시스템 적용, 에디터 코루틴 적용

---
+++ Runtime/Common/Flow.cs
@@ -0,0 +1,202 @@
+using System;
+using System.Collections.Generic;
+using Aya.Tween;
+using QuickEye.Utility;
+using UnityEngine;
+using NaughtyAttributes;
+using System.Collections;
+
+namespace GB
+{
+    public class Flow : MonoBehaviour
+    {
+        [ReorderableList]
+        [SerializeField] List<FlowData> _flowDatas;
+
+        float _time;
+
+
+        void OnDisable()
+        {
+            Stop();
+        }
+
+        public void Stop()
+        {
+#if UNITY_EDITOR
+
+            if (Application.isPlaying)
+            {
+                StopAllCoroutines();
+            }
+            else
+            {
+                GB.Edit.EditorCoroutines.StopAllCoroutines(this);
+            }
+
+#else
+
+            StopAllCoroutines();
+
+#endif
+
+        }
+
+
+
+        [Button]
+        public void Play()
+        {
+
+#if UNITY_EDITOR
+
+            if (Application.isPlaying)
+            {
+                StartCoroutine(Coroutine_Play());
+            }
+            else
+            {
+                GB.Edit.EditorCoroutines.StartCoroutine(Coroutine_Play(), this);
+            }
+
+#else
+
+            StartCoroutine(Coroutine_Play());
+
+#endif
+        }
+
+        IEnumerator Coroutine_Play()
+        {
+            List<float> timeList = new List<float>();
+            float prevTime = 0;
+            int curIDX = 0;
+            _time = 0;
+            for (int i = 0; i < _flowDatas.Count; ++i)
+            {
+
+                if (i == 0)
+                    prevTime = 0;
+                else
+                    prevTime = _flowDatas[i - 1].Time;
+
+                float time = _flowDatas[i].Time - prevTime;
+                if (time < 0) time = 0;
+                _time += time;
+
+                timeList.Add(time);
+            }
+
+
+            for (int i = 0; i < _flowDatas.Count; ++i)
+            {
+                if (_flowDatas[i] == null) continue;
+
+                yield return new WaitForSeconds(timeList[i]);
+                curIDX = i;
+                DataPlay(_flowDatas[i]);
+            }
+#if UNITY_EDITOR
+            if (!Application.isPlaying)
+            {
+                while (true)
+                {
+                    yield return new WaitForSeconds(0.2f);
+
+                    var data = _flowDatas[_flowDatas.Count - 1];
+
+                    if (curIDX == _flowDatas.Count - 1 && data.IsPlaying == false)
+                    {
+                        for (int i = 0; i < _flowDatas.Count; ++i)
+                        {
+                            if (_flowDatas[i].TweenAnim != null)
+                            {
+                                _flowDatas[i].TweenAnim.Tweener.Stop();
+                                _flowDatas[i].TweenAnim.Tweener.SetStart();
+                            }
+
+                        }
+
+                        yield break;
+                    }
+                }
+            }
+#endif
+
+        }
+
+        void DataPlay(FlowData data)
+        {
+            if (data == null) return;
+
+            if (data.TweenAnim != null)
+            {
+                if (Application.isPlaying)
+                {
+                    data.TweenAnim.Play();
+
+                }
+                else
+                {
+                    data.TweenAnim.SyncTweenerParams();
+
+
+
+                    if (!Application.isPlaying)
+                    {
+                        TweenManager.Ins.PreviewTweenerList.Add(data.TweenAnim.Tweener);
+                    }
+                    data.TweenAnim.Tweener
+                        .SetPlayCallback(() =>
+                        {
+
+                            // EnableEditor = false;
+                        })
+                        .SetStopCallback(() =>
+                        {
+                            // data.TweenAnim.Tweener.SetStart();
+
+                        })
+                        .Play();
+
+                }
+            }
+
+            if (data.Particle != null)
+            {
+                data.Particle.Play();
+            }
+
+        }
+    }
+
+    [Serializable]
+    public class FlowData
+    {
+        public bool IsPlaying
+        {
+            get
+            {
+                bool isPlaying = false;
+                if (TweenAnim != null)
+                {
+                    if (TweenAnim.Tweener != null)
+                        isPlaying = TweenAnim.Tweener.IsPlaying;
+                }
+
+                if (!isPlaying && Particle != null)
+                {
+                    isPlaying = Particle.isPlaying;
+                }
+
+                return isPlaying;
+
+            }
+
+        }
+        public float Time;
+        public TweenAnimation TweenAnim;
+        public ParticleSystem Particle;
+
+    }
+}
---
+++ Util/UTween-master/Core/Script/Editor/Base/TweenVector4BaseEditor.cs
@@ -0,0 +1,38 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+//
+//  Script   : TweenVector4BaseEditor.cs
+//  Info     : TweenVector4Base 编辑器
+//  Author   : ls9512 2021
+//  E-mail   : ls9512@vip.qq.com
+//
+/////////////////////////////////////////////////////////////////////////////
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+namespace Aya.Tween
+{
+    public abstract class TweenVector4BaseEditor : TweenerTVCEditor
+    {
+        public new Tweener<Vector4> Tweener => Target as Tweener<Vector4>;
+
+        public override void DoDrawValue()
+        {
+            base.DoDrawValue();
+            EditorGUILayout.BeginVertical();
+            FromVector4Property.vector4Value = EditorGUILayout.Vector4Field("From", FromVector4Property.vector4Value);
+            ToVector4Property.vector4Value = EditorGUILayout.Vector4Field("To", ToVector4Property.vector4Value);
+            EditorGUILayout.EndVertical();
+        }
+
+        public override bool DoDrawCallback()
+        {
+            base.DoDrawCallback();
+            if (!IsCallbackOpen) return false;
+
+            EditorGUILayout.PropertyField(OnValueVector4Property);
+            return true;
+        }
+    }
+}
+#endif
