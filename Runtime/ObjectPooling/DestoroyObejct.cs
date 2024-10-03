dd1ef28a8deef5f490def36fd892c7fb19f9ad77
shogun0331 <shogun0331@gmail.com>
2024-10-03 17:28:22 +0900

    Update DestoroyObejct.cs

--- Runtime/ObjectPooling/DestoroyObejct.cs
+++ Runtime/ObjectPooling/DestoroyObejct.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class DestoroyObejct : MonoBehaviour
 {
     public float EndTime = 2.0f;
-    
+
+    float _time;
     private void OnEnable() 
     {
-        GB.Timer.Create(EndTime, () => { GB.ObjectPooling.Destroy(this.gameObject); });
-
+        _time = 0;
         
     }
-
-
+    void Update()
+    {
+        _time += Time.deltaTime;
+        if(_time > EndTime)
+        {
+            GB.ObjectPooling.Destroy(this.gameObject);
+        }
+    }
 }
