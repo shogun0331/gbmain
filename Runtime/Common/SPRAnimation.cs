d58797a330c242b43b35ad507e4d87077375d57c
shogun0331 <shogun0331@gmail.com>
2024-10-14 11:00:02 +0900

    SPR Animation FIX

--- Runtime/Common/SPRAnimation.cs
+++ Runtime/Common/SPRAnimation.cs
@@ -1,163 +1,83 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using NaughtyAttributes;
 using QuickEye.Utility;
 using UnityEngine;
-
+using UnityEngine.Events;
 namespace GB
 {
-[Serializable]
-public class SPRAnimation 
-{
-    public enum State{Play = 0,Next,End,Trigger}
-    [SerializeField] UnityDictionary<string,SPRAnimationData> _dictAnim;
-
-    SpriteRenderer _sprRender;
-
-    public bool IsPlaying {get { return _isPlaying;}}
-    bool _isPlaying;
-    
-    string _curID;
-    
-    public string AnimationName{get{return _curID;}}
-    int _curIDX;
-    public int AnimationIndex{get{return _curIDX;}}
-
-    bool _isLoop;
-    public bool IsLoop{get{return _isLoop;}}
-    float _fixTimer;
-
-    Action<State,int,TriggerData> _callBack;
-    const float TIMER = 0.05f;
-
-    
-
-    
-
-    public void Init(SpriteRenderer spriteRenderer,Action<State,int,TriggerData> callBack)
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class SPRAnimation : MonoBehaviour
     {
-        _sprRender = spriteRenderer;
-        _callBack = callBack;
-    }
+        [SerializeField] string _animationName;
+        [SerializeField] UnityDictionary<string,SPRAnimationData> _animations = new UnityDictionary<string, SPRAnimationData>();
 
-    public void Play(float speed = 1)
-    {
-        if(_sprRender == null) 
+        SPRAnimationClip _sprAnimations;
+
+        public bool PlayAutomatically;
+        
+        Action<SPRAnimationClip.State,int,TriggerData> _callBack;
+
+        public UnityEvent onStartEvent;
+        public UnityEvent onEndEvent;
+
+        void Awake()
         {
-              Debug.LogWarning("Animation Init(SpriteRenderer spriteRenderer,Action<State,int,TriggerData> callBack) : null");
-            return;
-        }
-        if(_dictAnim.Count <= 0) return;
-        if(string.IsNullOrEmpty(_curID))
-        {
-            foreach(var v in _dictAnim) 
+            _sprAnimations = new SPRAnimationClip();
+            _sprAnimations.Init(GetComponent<SpriteRenderer>(),OnCallBack);
+            foreach(var v in _animations)
             {
-                _curID = v.Key;
-                _curIDX = 0;
-                break;
+                _sprAnimations.AddSPRAnimationData(v.Key,v.Value);
             }
         }
-      
-        if(_dictAnim[_curID].SpriteCount <= 0) return;
-        _curIDX = 0;
-        _sprRender.sprite = _dictAnim[_curID].GetSprite( _curIDX);
-        _isPlaying = true;
-        _isLoop = _dictAnim[_curID].IsLoop;
-        if(_dictAnim[_curID].Speed <= 0) _dictAnim[_curID].Speed = 1;
-        _fixTimer =  TIMER / (_dictAnim[_curID].Speed * speed);
-        _callBack?.Invoke(State.Play,_curIDX,null);
-    }
 
-    public void Play(string id,float speed = 1)
-    {
-        if(_sprRender == null) 
+        void OnEnable()
         {
-              Debug.LogWarning("Animation Init(SpriteRenderer spriteRenderer,Action<State,int,TriggerData> callBack) : null");
-            return;
-        }
 
-        if(_dictAnim.Count <= 0) return;
-        if(!_dictAnim.ContainsKey(id)) return;
-        if(_dictAnim[id].SpriteCount <= 0) return;
-
-        _curID = id;
-        _curIDX = 0;
-        _sprRender.sprite = _dictAnim[_curID].GetSprite(0);
-        
-        _isPlaying = true;
-        _isLoop = _dictAnim[_curID].IsLoop;
-        if(_dictAnim[_curID].Speed <= 0) _dictAnim[_curID].Speed = 1;
-        _fixTimer =  TIMER / (_dictAnim[_curID].Speed * speed);
-        _callBack?.Invoke(State.Play,_curIDX,null);
-    }
-    
-    public void Stop()
-    {
-        if(_sprRender == null) 
-        {
-              Debug.LogWarning("Animation Init(SpriteRenderer spriteRenderer,Action<State,int,TriggerData> callBack) : null");
-            return;
-        }
-
-        _isPlaying = false;
-        _callBack?.Invoke(State.Play,_curIDX,null);
-        
-    }
-
-    public void Resume()
-    {
-        if(_sprRender == null) 
-        {
-              Debug.LogWarning("Animation Init(SpriteRenderer spriteRenderer,Action<State,int,TriggerData> callBack) : null");
-            return;
-        }
-        _isPlaying = true;
-    }
-
-    float _time;
-    
-
-    public void Update(float dt)
-    {
-        if(_isPlaying == false) return;
-        if(_sprRender == null) 
-        {
-              Debug.LogWarning("Animation Init(SpriteRenderer spriteRenderer,Action<State,int,TriggerData> callBack) : null");
-            return;
-        }
-        
-        _time += dt;
-
-        if(_time > _fixTimer)
-        {
-            _time = 0;
-            _curIDX ++;
-
-            if(_curIDX >= _dictAnim[_curID].SpriteCount )
+            if(PlayAutomatically)
             {
-                if(_isLoop)
-                {
-                    _curIDX = 0;
-                    _sprRender.sprite = _dictAnim[_curID].GetSprite(_curIDX);
-                    //_callBack?.Invoke(State.Next,_curIDX,string.Empty);
-                }
-                else
-                {
-                    Stop();
-                    return;
-                }
+                Play(_animationName);
             }
-
-            _sprRender.sprite = _dictAnim[_curID].GetSprite(_curIDX);
+        
             
-            if(_dictAnim[_curID].Triggers.ContainsKey(_curIDX))
+        }
+        public void AddListener(Action<SPRAnimationClip.State,int,TriggerData> callback)
+        {
+            _callBack = callback;
+        }
+
+        public virtual void OnCallBack(SPRAnimationClip.State state,int index,TriggerData trigger)
+        {
+            if(state == SPRAnimationClip.State.Play)
             {
-                if(_dictAnim[_curID].Triggers != null)
-                {
-                    for(int i = 0; i< _dictAnim[_curID].Triggers[_curIDX].Count;++i)
-                        _callBack?.Invoke(State.Trigger,_curIDX,_dictAnim[_curID].Triggers[_curIDX][i]);
-                }
+                onStartEvent?.Invoke();
             }
+            else if(state == SPRAnimationClip.State.End)
+            {
+                onEndEvent?.Invoke();
+            }
+            
+            _callBack?.Invoke(state,index,trigger);
+        }
+        
+
+        public void Play(string animationName)
+        {
+            if(!_animations.ContainsKey(animationName)) return;
+            _animationName = animationName;
+
+            _sprAnimations.Play(_animationName);
+        }
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(_sprAnimations != null)
+                _sprAnimations.Update(Time.deltaTime);
+            
+
         }
     }
-}
-
 }
