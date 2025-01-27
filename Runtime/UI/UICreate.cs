d4bb4c7549378eaaa3a250e476d59d7d90e2afc4
shogun0331 <shogun0331@gmail.com>
2025-01-27 23:04:12 +0900

    Update UICreate.cs

--- Runtime/UI/UICreate.cs
+++ Runtime/UI/UICreate.cs
@@ -162,83 +162,83 @@ namespace GB
 
         }
 
-        public string AnimationName;
+        // public string AnimationName;
 
-        [Button]
-        public void CreateAnimation()
-        {
-            if(string.IsNullOrEmpty( UIName)) return;
-            if(string.IsNullOrEmpty( AnimationName)) return;
+        // [Button]
+        // public void CreateAnimation()
+        // {
+        //     if(string.IsNullOrEmpty( UIName)) return;
+        //     if(string.IsNullOrEmpty( AnimationName)) return;
 
-            if(GetComponent<UIScreen>() == null) return;
-            if(GetComponent<Animation>() == null) gameObject.AddComponent<Animation>();
+        //     if(GetComponent<UIScreen>() == null) return;
+        //     if(GetComponent<Animation>() == null) gameObject.AddComponent<Animation>();
             
-            var obj = Resources.Load<AnimationClip>("UIAnimation");
+        //     var obj = Resources.Load<AnimationClip>("UIAnimation");
 
-            string oriPath = AssetDatabase.GetAssetPath(obj);
-            string directory = GetComponent<UIScreen>().UIType == ScreenType.SCENE ? "Scene":"Popup";
-            string p = Application.dataPath+"/"+EDITOR_SAVECS_PATH.Replace("$PATH$", "Animation/UI/"+ directory).Replace("$FILENAME$", UIName);
+        //     string oriPath = AssetDatabase.GetAssetPath(obj);
+        //     string directory = GetComponent<UIScreen>().UIType == ScreenType.SCENE ? "Scene":"Popup";
+        //     string p = Application.dataPath+"/"+EDITOR_SAVECS_PATH.Replace("$PATH$", "Animation/UI/"+ directory).Replace("$FILENAME$", UIName);
 
-            DirectoryInfo info = new DirectoryInfo(p);
-            if (info.Exists == false) info.Create();
+        //     DirectoryInfo info = new DirectoryInfo(p);
+        //     if (info.Exists == false) info.Create();
 
-            string path = "Assets/Animation/UI/"+ directory+"/" +  UIName+"/"+ AnimationName+ ".anim";
-            bool isSuccess = AssetDatabase.CopyAsset(oriPath, path);  
-            AssetDatabase.Refresh();
+        //     string path = "Assets/Animation/UI/"+ directory+"/" +  UIName+"/"+ AnimationName+ ".anim";
+        //     bool isSuccess = AssetDatabase.CopyAsset(oriPath, path);  
+        //     AssetDatabase.Refresh();
 
-            LoadAnimation();
+        //     LoadAnimation();
             
-            EditorApplication.ExecuteMenuItem("Window/Animation/Animation"); 
+        //     EditorApplication.ExecuteMenuItem("Window/Animation/Animation"); 
 
-        }
-        [Button]
-        void LoadAnimation()
-        {
+        // }
+        // [Button]
+        // void LoadAnimation()
+        // {
 
-            string directory = GetComponent<UIScreen>().UIType == ScreenType.SCENE ? "Scene":"Popup";
-            string p = Application.dataPath+"/"+EDITOR_SAVECS_PATH.Replace("$PATH$", "Animation/UI/"+ directory).Replace("$FILENAME$", UIName);
+        //     string directory = GetComponent<UIScreen>().UIType == ScreenType.SCENE ? "Scene":"Popup";
+        //     string p = Application.dataPath+"/"+EDITOR_SAVECS_PATH.Replace("$PATH$", "Animation/UI/"+ directory).Replace("$FILENAME$", UIName);
 
-            DirectoryInfo info = new DirectoryInfo(p);
-            if (info.Exists == false) return;
+        //     DirectoryInfo info = new DirectoryInfo(p);
+        //     if (info.Exists == false) return;
 
             
-            List<string> fileList = new List<string>();
+        //     List<string> fileList = new List<string>();
 
-            foreach (System.IO.FileInfo File in info.GetFiles())
-            {
-                if (File.Extension.ToLower().CompareTo(".anim") == 0)
-                {
-                    string FullFileName = File.FullName;
-                    int len = FullFileName.Length - Application.dataPath.Length;
-                    fileList.Add("Assets" + FullFileName.Substring(Application.dataPath.Length, len));
-                }
-            }
+        //     foreach (System.IO.FileInfo File in info.GetFiles())
+        //     {
+        //         if (File.Extension.ToLower().CompareTo(".anim") == 0)
+        //         {
+        //             string FullFileName = File.FullName;
+        //             int len = FullFileName.Length - Application.dataPath.Length;
+        //             fileList.Add("Assets" + FullFileName.Substring(Application.dataPath.Length, len));
+        //         }
+        //     }
 
-            List<AnimationClip> animList = new List<AnimationClip>();
+        //     List<AnimationClip> animList = new List<AnimationClip>();
 
-            for (int i = 0; i < fileList.Count; ++i)
-            {
-                UnityEngine.Object[] data = AssetDatabase.LoadAllAssetsAtPath(fileList[i]);
-                foreach (UnityEngine.Object v in data)
-                {
-                    if (v.GetType() == typeof(AnimationClip))
-                    {
-                        animList.Add((AnimationClip)v);
-                    }
-                }
-            }
+        //     for (int i = 0; i < fileList.Count; ++i)
+        //     {
+        //         UnityEngine.Object[] data = AssetDatabase.LoadAllAssetsAtPath(fileList[i]);
+        //         foreach (UnityEngine.Object v in data)
+        //         {
+        //             if (v.GetType() == typeof(AnimationClip))
+        //             {
+        //                 animList.Add((AnimationClip)v);
+        //             }
+        //         }
+        //     }
             
             
-            DestroyImmediate(GetComponent<Animation>());
-            GetComponent<UIScreen>().ClearAnim();
+        //     DestroyImmediate(GetComponent<Animation>());
+        //     GetComponent<UIScreen>().ClearAnim();
 
-            var anim = gameObject.AddComponent<Animation>();
-            for(int i = 0; i< animList.Count; ++i)
-            {
-                anim.AddClip(animList[i],animList[i].name);
-                GetComponent<UIScreen>().Add(animList[i].name,animList[i]);
-            }
-        }
+        //     var anim = gameObject.AddComponent<Animation>();
+        //     for(int i = 0; i< animList.Count; ++i)
+        //     {
+        //         anim.AddClip(animList[i],animList[i].name);
+        //         GetComponent<UIScreen>().Add(animList[i].name,animList[i]);
+        //     }
+        // }
 
 
         [Button]
