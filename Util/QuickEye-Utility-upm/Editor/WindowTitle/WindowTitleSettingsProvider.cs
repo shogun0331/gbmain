d027bf0dd6bb1cea2fdc2083b463e96c6939f0dc
shogun0331 <shogun0331@gmail.com>
2025-02-14 14:05:17 +0900

    Update WindowTitleSettingsProvider.cs

--- Util/QuickEye-Utility-upm/Editor/WindowTitle/WindowTitleSettingsProvider.cs
+++ Util/QuickEye-Utility-upm/Editor/WindowTitle/WindowTitleSettingsProvider.cs
@@ -1,20 +1,20 @@
-﻿#if UNITY_SETTINGS_MANAGER
-using UnityEditor;
-using UnityEditor.SettingsManagement;
+﻿// #if UNITY_SETTINGS_MANAGER
+// using UnityEditor;
+// using UnityEditor.SettingsManagement;
 
-namespace QuickEye.Utility.Editor.WindowTitle
-{
-    internal static class WindowTitleSettingsProvider
-    {
-        [SettingsProvider]
-        private static SettingsProvider CreateSettingsProvider()
-        {
-            var provider = new UserSettingsProvider("Preferences/Window Title",
-                WindowTitleSettings.Instance,
-                new[] { typeof(WindowTitleSettingsProvider).Assembly });
+// namespace QuickEye.Utility.Editor.WindowTitle
+// {
+//     internal static class WindowTitleSettingsProvider
+//     {
+//         [SettingsProvider]
+//         private static SettingsProvider CreateSettingsProvider()
+//         {
+//             var provider = new UserSettingsProvider("Preferences/Window Title",
+//                 WindowTitleSettings.Instance,
+//                 new[] { typeof(WindowTitleSettingsProvider).Assembly });
 
-            return provider;
-        }
-    }
-}
-#endif
+//             return provider;
+//         }
+//     }
+// }
+// #endif
